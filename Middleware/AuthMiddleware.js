ae8445dba94e7e3c58cf13113af51ae73f4391e9
THISGUYREGGSCODES <109843198+thisguyreggscodes@users.noreply.github.com>
2024-02-01 00:43:03 +0800

    update auth

--- Middleware/AuthMiddleware.js
+++ Middleware/AuthMiddleware.js
@@ -1,16 +1,39 @@
+import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import User from "../Models/UserModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-  // Skip token verification for development/testing purposes
-  const userId = req.query.userId;  // Assuming you include userId in the query parameter for testing
+  let token;
 
-  if (userId) {
-    req.user = await User.findById(userId).select("-password");
-    next();
-  } else {
-    res.status(401).json({ message: "Not authorized, userId missing" });
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an Admin");
+  }
+};
+export { protect, admin };
